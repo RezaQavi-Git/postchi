f1d01ec9a49a424ff46b283f052ad0a80d63d79c
ReZa QaVi <reza.qavi@tapsi.cab>
2025-03-28 13:20:21 +0330

    config for flask session

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from app.routes import main
+import os
+from config import Config
 
 def create_app():
     app = Flask(__name__)
-    
+
+    app.secret_key = Config.SECRET_KEY
     
     app.register_blueprint(main)
 
--- config.py
+++ config.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "random secret")
     SERVER_PORT = os.getenv("SERVER_PORT", 5000)
     
